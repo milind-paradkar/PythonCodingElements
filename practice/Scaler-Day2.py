28547c5d3601d8ef32beeca169d725b9a12e9345
milind-paradkar <milind.paradkar@pfizer.com>
2023-07-28 11:53:30 +0530

    Day 2 practice added

---
+++ practice/Scaler-Day2.py
@@ -0,0 +1,20 @@
+print("Welcome to Day2!")
+''' This is multi-line comment
+s = input("Enter any number or str to show multiplication (*) by default all input is considered as str only:")
+print(s * 5)
+s = input("Enter any number or str to show multiplication (*):")
+print(s * 5)
+i = int(input("Enter any number to show multiplication (*):"))
+print(i * 5)
+div = int(input("Any number to show division:"))
+print(div/2, " is considered as by default:", type(div))
+div = int(input("Any number to show division:"))
+print(div//2, " // is known as floor division and it always returns:", type(div))
+div = int(input("Any negative number to show division:"))
+print(div//2, "// is known as floor division and it results in int, but floored (-5 for -4.1 or -4.9) for negative "
+              "number")
+'''
+print('In Python, for concatenating strings in long statements'
+      'does not require + to add multiple separate strings.'
+      ' You see?' ' Two statements can be simply added one after another.'
+      '\n ** known as power operator', 4 ** 2)
