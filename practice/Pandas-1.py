4098a21e1898a9a4c095e53e3e31a6db034fe89b
Milind Paradkar <89004620+milind-paradkar@users.noreply.github.com>
2024-12-03 12:05:30 +0530

    Pandas-1 updated

--- practice/Pandas-1.py
+++ practice/Pandas-1.py
@@ -15,67 +15,94 @@ def prln(*kwargs):
 # rowCount = 2
 
 df = pd.read_csv("mckinsey.csv")
+# pd.read_excel()
+# pd.read_json()
+# pd.read_xml()
+'''
+CSV: pd.read_csv('file.csv') 
+Excel: pd.read_excel('file.xlsx') 
+JSON: pd.read_json('file.json') 
+HTML: pd.read_html('file.html') 
+SQL: pd.read_sql('SQL_query', connection) 
+Parquet: pd.read_parquet('file.parquet') 
+HDF5: pd.read_hdf('file.h5') 
+Feather: pd.read_feather('file.feather') 
+Stata: pd.read_stata('file.dta') 
+SAS: pd.read_sas('file.sas7bdat') 
+SPSS: pd.read_spss('file.sav')
+'''
 prln("df:\n", df)
 prln("\ndf.size:", df.size)
 prln("df.shape (a tuple):", df.shape)
-prln("df.shape[0] (rows) * df.shape[1] (columns) :", df.shape[0] * df.shape[1], "= df.size:", df.size)
+prln("df.shape[0] (rows) * df.shape[1] (columns) :", df.shape[0], "*", df.shape[1], "= df.size:", df.size)
 prln("\ntype(df):", type(df))
 prln("Series: Single row or column")
 
-prln("DF series:", type(df['life_exp']))
-prln(df['life_exp'], "\n")
-
+prln("DF series type:", type(df['life_exp']))
+prln("This results naturally as Series:\n", df['life_exp'])
+lst = ['life_exp']
+prln("This is df with only one column:\n", df[lst])
+prln("df.column_name (not a good way to access column):\n", df.life_exp)
 prln(df.head())  # Top 5 by default
 prln(df.head(2))
 prln(df.tail())  # Bottom 5 by default
 prln(df.tail(10))
+print("\ndf.info() will give you:")
 df.info()
 prln(type(df['life_exp'][0]), df['life_exp'][0])
 prln(type(df['life_exp'][0:3]), df['life_exp'][0:3])
 prln(df['life_exp'][10:20])
 
-prln(df.columns)
-prln(df.keys())
-prln(df['life_exp'].keys(), "--this has different meaning than df.keys()")
+prln("df.columns: ", df.columns)
+prln("df.keys():  ", df.keys())
+prln("17] series.keys():  ", df['life_exp'].keys(), "--this has different meaning than df.keys()")
 # prln(df['country', 'year']) # Gives error
 prln(df[['country', 'year']])  # Passing list of columns to df
 prln(df[['country', 'year']][2:4])  # Passing list of columns to df
-prln(df['country'].unique())
-prln(df['country'].nunique())
-prln(df['country'].values)
-prln(df['country'].value_counts())
-df.rename({'country': 'Country_changed'})  # Rename supports dict. "key":"value"
-df_goodnames = df.rename({'country': 'Country_changed'})  # Rename supports dict. "key":"value"
-prln(df_goodnames)
+prln("22:\n", df['country'].unique())
+prln("24:\n", df['country'].nunique())
+prln("25:\n", df['country'].values)
+prln("26:\n", len(df['country'].values))
+prln("value_counts:\n", df['country'].value_counts())
+prln("value_counts for Zimbabwe:\n", df['country'].value_counts()['Zimbabwe'])
+df.rename({'country': 'Country_changed'})  # Rename supports dict. {"key":"value"}
+df_good_names = df.rename({'country': 'Country_changed'})  # Rename supports dict. "key":"value"
+prln(df_good_names)
 prln("It did not change column names .. Why?")
 print('Numpy Axis: 0:col, 1:row')
 print('but in Pandas, ')
 print('Pandas Axis: 0: row, 1:column')
 print("Let's use this concept..")
-df_goodnames = df.rename({0: 2222})
-prln(df_goodnames)
-df_goodnames = df.rename({'country': 'Country_changed'}, axis=1)  # Rename supports dict. "key":"value"
-prln(df.keys(), df_goodnames.keys(), df_goodnames)
-df_goodnames = df.rename({'country': 'Country_changed', "year": 'Year_changed'},
-                         axis=1)  # Rename supports dict. "key":"value"
-prln(df_goodnames)
+df_good_names = df.rename({1: 2222})
+prln("Here row with id 1(starting from 0) is changed to 2222\n", df_good_names.head())
+df_good_names = df.rename({'country': 'Country_changed'}, axis=1)  # Rename supports dict. "key":"value"
+prln(df_good_names)
+prln("33]", df.keys(), "\n", df_good_names.keys(), '\n', df_good_names)
+df_good_names = df.rename({'country': 'Country_changed', "year": 'Year_changed'},
+                          axis=1)  # Rename supports dict. "key":"value"
+prln(df_good_names)
 df.rename({'country': 'Country_changed', "continent": 'Continent_changed'}, axis=1, inplace=True)
 prln(df)
+prln(df.rename(columns={"year": "year_new", "life_exp": "life_expectancy"}, inplace=True))
+df.rename({"gdp_cap": "gdp_capacity"}, axis="columns", inplace=True)
 prln("Dropping row/column")
-df_goodnames.drop(0)  # Drops a ROW (you thought a column?)
-prln(df_goodnames.head(2))
+df_good_names.drop(0)  # Drops a ROW (you thought a column?)
+prln(df_good_names.head(2))
 #df_goodnames.drop('Country_changed') # Trying to drop a column, but not successful. -- KeyError: "['Country_changed'] not found in axis"
-df_goodnames_less_cols = df_goodnames.drop('Country_changed', axis=1)  # Trying to drop a column, but not successful
-prln("***** ", df_goodnames.head(2), "\n\n", df_goodnames_less_cols.head(2))
-df_goodnames.drop('continent', axis=1, inplace=True)  # Trying to drop a column, in place
-prln(df_goodnames.head(2))
+df_good_names_less_cols = df_good_names.drop('Country_changed', axis=1)  # Trying to drop a column, but not successful
+df_good_names_less_cols = df_good_names.drop(['Country_changed', 'Year_changed'], axis=1)  # Trying to drop a column, but not successful
+prln(df_good_names_less_cols)
+exit()
+prln("***** ", df_good_names.head(2), "\n\n", df_good_names_less_cols.head(2))
+df_good_names.drop('continent', axis=1, inplace=True)  # Trying to drop a column, in place
+prln(df_good_names.head(2))
 
 print('****** inplace is NOT used, so original remains same, but new df is created.')
-df_goodnames_less_col_list = df_goodnames.drop(['Country_changed', 'Year_changed'],
-                                               axis=1)  # inplace is NOT used, so original remains same, but new df is created.
+df_goodnames_less_col_list = df_good_names.drop(['Country_changed', 'Year_changed'],
+                                                axis=1)  # inplace is NOT used, so original remains same, but new df is created.
 prln(df_goodnames_less_col_list)
-df_goodnames_less_col_list = df_goodnames.drop(['Country_changed', 'Year_changed'], axis=1,
-                                               inplace=True)  # inplace IS used, so original changed, but new df is not created --None.
+df_goodnames_less_col_list = df_good_names.drop(['Country_changed', 'Year_changed'], axis=1,
+                                                inplace=True)  # inplace IS used, so original changed, but new df is not created --None.
 print('inplace IS used, so original changed, but new df is NOT created --None.')
 prln(df_goodnames_less_col_list,
      "  --> df_goodnames_less_col_list is None. So anything operated on this such as .head() will give you ERROR.")  # df_goodnames_less_col_list is None. So anything operated on this such as .head() will give you ERROR.
@@ -112,8 +139,9 @@ prln("df.head(15):\n", df.head(15))
 prln("df.loc uses explicit index and all numbers are inclusive.")
 prln("-- df.loc[row,col] ----- col is optional")
 prln("df.loc[3:10]:\n", df.loc[3:10])
-prln(df.loc[len(df)-1::-3])
-prln(df.loc[[1,4,5,7]]) # Cannot simply use [,] syntax just to send rows.. it would be treated as [row, col], that's why [[rows series]]
+prln(df.loc[len(df) - 1::-3])
+prln(df.loc[[1, 4, 5,
+             7]])  # Cannot simply use [,] syntax just to send rows.. it would be treated as [row, col], that's why [[rows series]]
 prln(df.loc[1:7, :])
 prln(df.loc[10:15, :])
 prln(df.columns)
@@ -126,16 +154,11 @@ prln(df.loc[10:15, ['year', 'population']])
 s = pd.Series([76, 5, 3, 7, 8, 9])
 s = pd.Series([76, 5, 3, 7, 8, 9], name='Random_Series')
 prln("series:s: (implicit and explicit indexes are same)\n", s)
-s.index = ['a','b','c','d','e','f']
-prln("Series:s ->\n",s)
-prln(s.loc['b':'e']) # As this is Series, it has only row index (no column index)
+s.index = ['a', 'b', 'c', 'd', 'e', 'f']
+prln("Series:s ->\n", s)
+prln(s.loc['b':'e'])  # As this is Series, it has only row index (no column index)
 prln("this does not work:['e':'b']", s.loc['e':'b'])
 prln(s.loc[['c', 'a']])
-
-
-
-
-
 
 # Average 0.9480547904968262 seconds on Lenovo as against 0.14976930618286133 seconds on HP Pavillion
 print("---Whole execution completed in %s seconds ---" % (time.time() - start_time))
